fd42ffbca8ea547f81daaeb0e224a0e1a82cd11f
mdshahadad <identity.shahadad@gmail.com>
2024-12-18 14:20:41 +0600

    Make built in module

---
+++ index.js
@@ -0,0 +1,6 @@
+const { a, add } = require('./local.js')
+const { a: a2, add: add2 } = require('./local-1.js')
+
+// add(2,3)
+
+console.log(add(2, 3), add2(2, 3, 5));
---
+++ local-1.js
@@ -0,0 +1,4 @@
+const add = (p1, p2, p3) => p1 + p2 + p3;
+a = 20;
+
+module.exports = { add, a };
